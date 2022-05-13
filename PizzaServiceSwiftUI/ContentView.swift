4c2c2b7132591abc469e7dbf20aa9d125f26fb2a
Starsky <starsky.m@live.com>
2022-05-13 06:29:45 +0200

    Initial Commit

---
+++ PizzaServiceSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaServiceSwiftUI
+//
+//  Created by star on 13.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaServiceSwiftUI/PizzaServiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaServiceSwiftUIApp.swift
+//  PizzaServiceSwiftUI
+//
+//  Created by star on 13.05.22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaServiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
